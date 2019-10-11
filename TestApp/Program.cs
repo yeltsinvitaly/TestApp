042a4b4ad48319abc02bbc3b5e03dcfdee254d80
yeltsinvitaly <yeltsinvitaly@gmail.com>
2019-10-11 04:20:11 +0300

    Добавьте файлы проекта.

---
+++ TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.WriteLine("Stop");
+            Console.ReadKey();
+        }
+    }
+}
